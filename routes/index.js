41bfc7979d2a1366c5198009b32b1a688b296e00
Ashish20sharma <133103644+Ashish20sharma@users.noreply.github.com>
2024-06-04 11:43:39 +0530

    setup all the routes and project completed

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,78 @@
 const express=require("express");
 const router=express.Router();
 const fs=require("fs");
+const path=require("path");
 
 router.get("/",function(req,res){
-    res.render("home");
+    fs.readdir(`./files`, (err, files) => {
+        if (err) {
+            return console.error(`Unable to scan directory: ${err}`);
+        }
+        res.render("home",{files});
+    });
 })
 
 router.get("/create",function(req,res){
     res.render("createHisaab");
 })
 
-router.get("/showhisaab",function(req,res){
-    res.render("showHisaab");
+router.get("/hisaab/:hisaabid",function(req,res){
+    const id=req.params.hisaabid;
+    fs.readdir("./files", (err, files) => {
+        if (err) {
+            return console.error(`Unable to scan directory: ${err}`);
+        }
+
+            const filePath = path.join("./files", id);
+
+            fs.readFile(filePath, 'utf8', (err, content) => {
+                if (err) {
+                    return console.error(`Error reading file ${filePath}: ${err}`);
+                }
+                res.render("showHisaab",{id,content});
+            });
+    });
 })
+
+router.get("/delete/:hisaabid",function(req,res){
+    const id=req.params.hisaabid;
+    fs.unlink(`./files/${id}`, (err) => {
+        if (err) {
+            console.error(`Error deleting file: ${err}`);
+            return;
+        }
+    });
+    res.redirect("/");
+})
+
+router.get("/edit/:hisaabid",function(req,res){
+    const id=req.params.hisaabid;
+    res.render("editHisaab",{id});
+});
+
+router.post("/edit/:hisaabid",function(req,res){
+    const {edithisaab}=req.body;
+    const id=req.params.hisaabid;
+    fs.readFile(`./files/${id}`, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading file: ${err}`);
+            return;
+        }
+    
+        const newContent = data + "," +edithisaab;
+    
+        fs.writeFile(`./files/${id}`, newContent, 'utf8', (err) => {
+            if (err) {
+                console.error(`Error writing file: ${err}`);
+                return;
+            }
+        });
+    });
+    res.redirect(`/hisaab/${id}`);
+})
+
 router.post("/create",(req,res)=>{
-    const {heading,hisaab}=req.body;
+    const {hisaab}=req.body;
     const day = new Date().getDate();
     const month = new Date().getMonth() + 1; // Months are zero-based, so add 1
     const year = new Date().getFullYear(); // Get the full year
@@ -26,9 +84,17 @@ router.post("/create",(req,res)=>{
         if(err){
            return console.log("error in write file.")
         }
-        console.log("done");
     });
-    res.redirect("/showhisaab");
+    res.redirect("/");
+});
+
+router.post("/readfile",function(req,res){
+    fs.readdir(`./files`, (err, files) => {
+        if (err) {
+            return console.error(`Unable to scan directory: ${err}`);
+        }
+        res.render("showHisaab",{files});       
+    });
 })
 
 module.exports=router;
